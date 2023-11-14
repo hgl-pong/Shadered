0b9f478577e46f2f0249e151bbbb58dc54d71897
hgl <2319100642@qq.com>
2023-11-14 13:09:08 +0800

    Add new submodules

--- libs/SPIRV-VM/inc/spvm/ext/GLSL450.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SPRIV_VM_EXT_GLSL_450_H__
-#define __SPRIV_VM_EXT_GLSL_450_H__
-
-#include <spvm/opcode.h>
-
-spvm_ext_opcode_func* spvm_build_glsl450_ext();
-
-#endif // __SPRIV_VM_EXT_GLSL_450_H__
--- libs/SpvGenTwo/dis/test/test.bat
+++
@@ -1,8 +0,0 @@
-for %%f in (*.spv*) do (
-    SpvGenTwoDisassembler.exe %%f --assignids
-    IF %ERRORLEVEL% NEQ 0 (
-        pause
-    )
-)
-
-pause
